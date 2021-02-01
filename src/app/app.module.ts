754648465f4e77de7e88bb58357e52aa9a7ca46a
Alex Slavyansky <alex.slavyansky@gmail.com>
2021-02-01 16:56:16 +0300

    Initial setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PeopleListComponent } from './components/people-list/people-list.component';
+import { PeopleFormComponent } from './components/people-form/people-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PeopleFormComponent,
+    PeopleListComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule    
   ],
   providers: [],
   bootstrap: [AppComponent]
