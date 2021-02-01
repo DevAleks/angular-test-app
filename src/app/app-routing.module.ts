d7db65c8a38703998b52d1b9de819414c47cc3dd
Alex Slavyansky <alex.slavyansky@gmail.com>
2021-02-01 16:58:16 +0300

    Added routing setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { PeopleListComponent } from './components/people-list/people-list.component';
+import { PeopleFormComponent } from './components/people-form/people-form.component';
+
+
+const routes: Routes = [
+  { path: "", component: PeopleListComponent },
+  { path: "form", component: PeopleFormComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
