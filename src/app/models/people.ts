7964ad91d3fb7cb581e493ac3852b2ec94e30959
Alex Slavyansky <alex.slavyansky@gmail.com>
2021-02-04 22:05:59 +0300

    Added a surname field

--- src/app/models/people.ts
+++ src/app/models/people.ts
@@ -1,9 +1,13 @@
 export interface People {
     name: string;
+    surname: string; 
     phone: string;
-    fields?: Array<string>;
-    text?: string;
-    link?: string;
+    newFields?: Array<
+    {
+        phone?: string;
+        link?: string;
+        text?: string;        
+    }>;
 }
 // Оставляю необязательные поля fields, text и link для дальнейшего
 // расширения формы с помошью FormArray или FormBuilder
